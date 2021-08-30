73f802dd9988e3318437c52598d303b1829c592d
Sohag <sohagjabed10@gmail.com>
2021-08-30 10:41:53 +0530

    Insta Account

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class Config:
     API_ID = 1927002
     API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
     BOT_TOKEN = "1999704931:AAGNXi73OErzmIWuANzFSmYDGAMKG4m4xtI"
-    REMOVE_BG_TOKEN = "7kQ6yp8aaCNy6fq6uJNsi4di"
     MONGO = "mongodb+srv://instagrambot:instagrambot@cluster0.3grqz.mongodb.net/SJBots?retryWrites=true&w=majority"
     INSTA_USER = "_anonymous.hackerr_"
     INSTA_PASS = "ForBotPurpose"
