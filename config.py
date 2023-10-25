10251ec7aa08164bc7cc5ba0431e7b764ef5cb28
sohag02 <70505164+sohag02@users.noreply.github.com>
2023-10-25 14:56:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 class Config:
 
-    API_ID = 1927002
-    API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
-    BOT_TOKEN = "1999704931:AAEpi_SBI77qa3_QHVOnz1QuoZ6tCxdxwow"
-    MONGO = "mongodb+srv://instagrambot:instagrambot@cluster0.3grqz.mongodb.net/SJBots?retryWrites=true&w=majority"
-    INSTA_USER = "_anonymous.hackerr_"
-    INSTA_PASS = "ForBotPurpose"
+    # Add Your own config here
+    API_ID = 
+    API_HASH = 
+    BOT_TOKEN = 
+    MONGO = 
+    INSTA_USER = 
+    INSTA_PASS = 
